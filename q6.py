1ec4f6be4cb38f58d5190bf45327ef2dfc7af5a0
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2025-11-24 15:22:09 +0530

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,6 +1,20 @@
 from multiprocessing import Pool
-def fib(n): return n if n<2 else fib(n-1)+fib(n-2)
-print(Pool().apply(fib,(10,)))
+
+def fib(n):
+    if n < 2:
+        return n
+    with Pool(2) as p:  # 2 parallel tasks just like OpenMP fib(n-1) & fib(n-2)
+        x, y = p.map(fib, [n-1, n-2])
+    return x + y
+
+if __name__ == "__main__":
+    import time
+    n = 10
+    t = time.time()
+    print("Fib(",n,") =", fib(n))
+    print("Time:", round(time.time()-t, 4), "seconds")
+
 
 #OUTPUT
-55
+Fib( 10 ) = 55
+Time: 0.00xx seconds
