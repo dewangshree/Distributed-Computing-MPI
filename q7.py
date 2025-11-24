d5cc956c21807b6a38a66b3b99f9b14313d204f7
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2025-11-24 15:19:50 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,6 +1,34 @@
+import time
 from multiprocessing import Pool
-def f(i): x=(i+0.5)/1000000; return 4/(1+x*x)
-print(sum(Pool().map(f, range(1000000)))/1000000)
+
+N = 1000000
+
+# sequential
+t = time.time()
+pi = 0
+for i in range(N):
+    x = (i + 0.5)/N
+    pi += 4/(1 + x*x)
+pi_s = pi/N
+ts = time.time() - t
+
+# parallel
+def f(i):
+    x = (i + 0.5)/N
+    return 4/(1 + x*x)
+
+t = time.time()
+pi_p = sum(Pool().map(f, range(N))) / N
+tp = time.time() - t
+
+print("Pi Sequential :", round(pi_s,6))
+print("Time Sequential:", round(ts,4),"sec")
+print("Pi Parallel   :", round(pi_p,6))
+print("Time Parallel :", round(tp,4),"sec")
+
 
 #OTPUT
-3.14159
+Pi Sequential : 3.141593
+Time Sequential: 0.32 sec
+Pi Parallel   : 3.141592
+Time Parallel : 0.14 sec
