b9ecbbb0ed1a68020d554d9786e8546f586b1ff1
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2025-11-24 15:33:19 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,14 +1,20 @@
 from mpi4py import MPI
+
 comm = MPI.COMM_WORLD
 r = comm.Get_rank()
+
 if r == 0:
-    comm.send(99, dest=1)
-    print("Rank 0 Sent")
-else:
-    x = comm.recv(source=0)
-    print("Rank 1 Received", x)
+    data = 99
+    comm.send(data, dest=1)
+    print("Rank 0 sent:", data)
+
+elif r == 1:
+    data = comm.recv(source=0)
+    print("Rank 1 received:", data)
+
 
   
 # OUTPUT
-Rank 0 Sent
-Rank 1 Received 99
+Rank 0 sent: 99
+Rank 1 received: 99
+
